ef662afd51be2cf2f20125e6852fbeef1febbe3d
rebs-code <80966551+rebs-code@users.noreply.github.com>
2021-03-19 16:04:33 +0200

    Update common_word.py
    Simple program to print the three most common words in a txt file

--- common_word.py
+++ common_word.py
@@ -1,12 +1,24 @@
+import string
 fname = input("Insert file name: ")
 fhand = open(fname)
 words = dict()
+common = list()
 for line in fhand:
+    line = line.translate(str.maketrans('', '', string.punctuation))
     line.rstrip()
     w = line.split()
-    words[w] = words.get(w, 0) + 1
-print(words)
+    for i in w:
+        words[i] = words.get(i, 0) + 1
+#print(words, end="\n")
+
+for key,value in list(words.items()):
+    common.append((value,key))
+
+common.sort(reverse=True)
+#print(common, end="\n")
 
 
-#for key, val in list(d.items()):
-#print(val, key)
+print("The 3 most common words are: %s, %s, %s"%(common[0][1], common[1][1], common[2][1]))
+
+
+
