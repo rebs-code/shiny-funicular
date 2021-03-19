dbd216218eda7ebbf7ef40d317548cee9885b071
rebs-code <80966551+rebs-code@users.noreply.github.com>
2021-03-19 17:06:51 +0200

    Add files via upload
    a simple program to count the most common letters in a txt file

---
+++ common_letter.py
@@ -0,0 +1,23 @@
+import string
+fname = input("Insert file name: ")
+fhand = open(fname)
+words = dict()
+common = list()
+for line in fhand:
+    line = line.translate(str.maketrans('', '', string.punctuation))
+    line.rstrip()
+    w = line.split()
+    for i in w:
+        letter = i.split()
+        for n in i:
+            words[n] = words.get(n, 0) + 1
+#print(words, end="\n")
+
+for key,value in list(words.items()):
+    common.append((value,key))
+
+common.sort(reverse=True)
+#print(common, end="\n")
+
+
+print("The 3 most common words are: %s, %s, %s"%(common[0][1], common[1][1], common[2][1]))
